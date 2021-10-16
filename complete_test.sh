9c18fca7888e25cad93a780c759f3160b95a4904
nayananga <nayanangamuhandiram@gmail.com>
2021-10-16 20:29:09 +0530

    updated for initial experiments

--- complete_test.sh
+++ complete_test.sh
@@ -19,15 +19,16 @@ for ConcurrencyPlan in ${ConcurrencyPlanArray[*]}
     for TestName in ${TestNameArray[*]}
       do
         docker run --publish 3306:3306 --name db_Server -d --net myNet --ip 192.168.1.6 --cpus=1 nayanagamuhandiram/mysql_service
-        echo "DbWrite"
-        docker run --publish 15000:15000 --name initial_experiments -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/initial_experiments/DbWrite/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:initial_exp java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar DbWrite 10 99P
+
+        echo "$TestName"
+        docker run --publish 15000:15000 --name initial_experiments -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/initial_experiments/DbWrite/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:initial_exp java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar "$TestName" 10 99P
         docker run --name jmeter_service -d --net myNet --ip 192.168.1.5 --cpus=1 nayanagamuhandiram/jmeter_service java -jar bin/ApacheJMeter.jar -n -t "$ConcurrencyPlan".jmx -q user.properties
         sleep 1200
         docker container kill jmeter_service initial_experiments
         docker container prune -f
 
-        echo "$TestName"
-        docker run --publish 15000:15000 --name initial_experiments -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/initial_experiments/DbWrite/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:initial_exp java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar "$TestName" 10 99P
+        echo "DbWrite"
+        docker run --publish 15000:15000 --name initial_experiments -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/initial_experiments/DbWrite/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:initial_exp java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar DbWrite 10 99P
         docker run --name jmeter_service -d --net myNet --ip 192.168.1.5 --cpus=1 nayanagamuhandiram/jmeter_service java -jar bin/ApacheJMeter.jar -n -t "$ConcurrencyPlan".jmx -q user.properties
         sleep 1200
         docker container kill jmeter_service initial_experiments
