1813a89f2dcb5d40c7543bbc1ea18a5693cc52ab
nayananga <nayanangamuhandiram@gmail.com>
2021-10-17 15:29:08 +0530

    updated for initial experiments

--- complete_test.sh
+++ complete_test.sh
@@ -10,7 +10,7 @@ docker container prune -f
 docker network prune -f
 docker network create --subnet=192.168.1.0/24 myNet
 
-declare -a TestNameArray=("Prime100k" "Prime1m" "Prime10m" "DbWrite" "DbRead")
+declare -a TestNameArray=("DbRead")
 declare -a ConcurrencyPlanArray=("1_concurrency" "10_concurrency" "50_concurrency" "increase_and_decrease" "one_step_function" "random" "step_increase_fucntion" "up_down_function" "peaks")
 
 for TestName in ${TestNameArray[*]}; do
@@ -18,10 +18,10 @@ for TestName in ${TestNameArray[*]}; do
   if [[ "$TestName" == "DbRead" ]]; then
     echo "im inside first if statement"
     docker run --publish 3306:3306 --name db_Server -d --net myNet --ip 192.168.1.6 --cpus=1 nayanagamuhandiram/mysql_service
-    sleep 1
+    sleep 15
     docker run --publish 15000:15000 --name initial_experiments -d --net myNet --ip 192.168.1.4 --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:initial_exp java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar DbWrite 10 99P
     docker run --name jmeter_service -d --net myNet --ip 192.168.1.5 --cpus=1 nayanagamuhandiram/jmeter_service java -jar bin/ApacheJMeter.jar -n -t 1_concurrency.jmx -q user.properties
-    sleep 5
+    sleep 60
     docker container kill jmeter_service initial_experiments
     docker container prune -f
   fi
