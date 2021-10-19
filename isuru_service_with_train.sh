a4ac60b802a16de58fd56f5f7dda7f30b8776ffe
nayananga <nayanangamuhandiram@gmail.com>
2021-10-19 18:41:03 +0530

    updated for isuru with train experiments

--- isuru_service_with_train.sh
+++ isuru_service_with_train.sh
@@ -2,7 +2,8 @@
 
 docker pull nayanagamuhandiram/mysql_service
 docker pull nayanagamuhandiram/jmeter_service
-docker pull nayanagamuhandiram/adaptive-concurrency-control:latest
+docker pull nayanagamuhandiram/java_service
+docker pull nayanagamuhandiram/python_service:hpc_testing_5
 
 docker container kill db_Server jmeter_service initial_experiments some-redis tuner_service java_service adapt-t_service
 docker container prune -f
@@ -32,10 +33,12 @@ for TestName in ${TestNameArray[*]}; do
       docker run --publish 3306:3306 --name db_Server -d --net myNet --ip 192.168.1.6 --cpus=1 nayanagamuhandiram/mysql_service
       sleep 1
     fi
-    docker run --publish 15000:15000 --name adapt-t_service -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/nilushan/"$TestName"/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/adaptive-concurrency-control:latest java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar "$TestName" 10 99P
+    docker run --publish 6379:6379 --name some-redis -d --net myNet --ip 192.168.1.2 --cpus=1 redis
+    docker run --publish 5000:5000 --name tuner_service -d --net myNet --ip 192.168.1.3 --cpus=1 nayanagamuhandiram/python_service:hpc_testing_5  python app.py "$TestName"/"$ConcurrencyPlan"
+    docker run --publish 15000:15000 --name java_service -d --net myNet --ip 192.168.1.4 --volume /home/nayananga/output/isuru/java_service/with_train/"$TestName"/"$ConcurrencyPlan":/home/log --cpus=1 nayanagamuhandiram/java_service java -jar adaptive-concurrency-control-1.0-SNAPSHOT-jar-with-dependencies.jar "$TestName" 10 99P
     docker run --name jmeter_service -d --net myNet --ip 192.168.1.5 --cpus=1 nayanagamuhandiram/jmeter_service java -jar bin/ApacheJMeter.jar -n -t "$ConcurrencyPlan".jmx -q user.properties
     sleep 1200
-    docker container kill jmeter_service adapt-t_service
+    docker container kill jmeter_service java_service tuner_service some-redis
     if [[ "$TestName" == "DbWrite" ]]; then
       echo "im inside third if statement"
       docker container kill db_Server
